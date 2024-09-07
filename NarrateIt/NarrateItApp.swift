55e416820f9af87fd233238fb9b6e4c473253a9d
vetri02 <vetri02@gmail.com>
2024-09-07 10:42:47 +0530

    Initial Commit

---
+++ NarrateIt/NarrateItApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarrateItApp.swift
+//  NarrateIt
+//
+//  Created by Vetrichelvan Jeyapalpandy on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NarrateItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
